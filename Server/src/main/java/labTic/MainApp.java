7372694b217501cd4aa510de6d66323ef0e5c706
Fernando Badano <ferbadano20@gmail.com>
2018-09-30 01:51:45 -0300

    Nueva GUI

--- Server/src/main/java/labTic/MainApp.java
+++ Server/src/main/java/labTic/MainApp.java
@@ -7,15 +7,21 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import labTic.services.entities.Client;
+import labTic.ui.AppController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class MainApp extends Application {
 
+    private AnnotationConfigApplicationContext context;
+    private Parent root;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent fxml = FXMLLoader.load(getClass().getResource("app.fmxl"));
+/*        Parent fxml = FXMLLoader.load(getClass().getResource("app.fmxl"));
 
         Scene scene = new Scene(fxml);
 
@@ -25,14 +31,23 @@ public class MainApp extends Application {
 
         primaryStage.initStyle(StageStyle.TRANSPARENT);
 
+        primaryStage.show();*/
+
+        context = new AnnotationConfigApplicationContext(MainApp.class);
+
+        FXMLLoader loader = new FXMLLoader(AppController.class.getResource("App.fxml"));
+        loader.setControllerFactory(context::getBean);
+
+        root = loader.load();
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
     }
 
-    public static void main(String... args){
+/*    public static void main(String... args){
 
         launch(args);
 
-    }
+    }*/
 
 }
--- Server/src/main/java/labTic/ui/AppController.java
+++ Server/src/main/java/labTic/ui/AppController.java
@@ -1,15 +1,19 @@
 package labTic.ui;
 
 import javafx.fxml.Initializable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+@Component
 public class AppController implements Initializable {
+
+    @Autowired
+    private Main main;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-
-
     }
 }
