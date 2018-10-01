b3575cafaa013822f57e56772fe48d0eef3cbb9e
Rolo <rodri.lopez98@gmail.com>
2018-09-30 23:11:28 -0300

    Arreglo en clase Main

--- Server/src/main/java/labTic/Main.java
+++ Server/src/main/java/labTic/Main.java
@@ -31,6 +31,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        cs = new ClientService();
         primaryStage.setTitle("Formulario");
 
         GridPane grid = new GridPane();
