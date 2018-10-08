f23a31160a27c8aa01e7c0b0afbb163a2c4270a4
Fernando Badano <ferbadano20@gmail.com>
2018-10-07 22:11:38 -0300

    Restaurante GUI

---
+++ Server/src/main/java/labTic/ui/RestaurantController.java
@@ -0,0 +1,21 @@
+package labTic.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class RestaurantController {
+
+
+    @FXML
+    private Text text3;
+
+    @FXML
+    private Text text1;
+
+    @FXML
+    private Text text2;
+
+    @FXML
+    private Text text4;
+
+}
