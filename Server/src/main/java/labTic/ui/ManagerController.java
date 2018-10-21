3b856195700b4499f395c36ae329d1fcd05374c8
Fernando Badano <ferbadano20@gmail.com>
2018-10-21 17:05:20 -0300

    Manager GUI

---
+++ Server/src/main/java/labTic/ui/ManagerController.java
@@ -0,0 +1,42 @@
+package labTic.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ManagerController {
+
+    @FXML
+    private TextField user;
+
+    @FXML
+    private PasswordField pass;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    void buscarRestaurantTab(MouseEvent event) {
+
+    }
+
+    @FXML
+    void nuevoRestaurantTab(MouseEvent event) {
+
+    }
+
+    @FXML
+    void registrarButton(MouseEvent event) {
+
+    }
+}
