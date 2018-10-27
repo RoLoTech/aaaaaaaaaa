df03b3ef2fa23d3ba368b4df9c1894b61acb610b
Fernando Badano <ferbadano20@gmail.com>
2018-10-27 19:29:38 -0300

    RestaurantView GUI

---
+++ Server/src/main/java/labTic/ui/ReservasController.java
@@ -0,0 +1,26 @@
+package labTic.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class ReservasController {
+
+
+    @FXML
+    private TableView<?> table1;
+
+    @FXML
+    private TableView<?> table2;
+
+    @FXML
+    void clickTable1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clickTable2(MouseEvent event) {
+
+    }
+
+}
