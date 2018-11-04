43f9e2ad6f3a819be735816a15ed4d3829e6f65e
Fernando Badano <ferbadano20@gmail.com>
2018-11-04 01:17:49 -0300

    GUI Login Restauranttttttt t t t tt t

---
+++ Server/src/main/java/labTic/ui/LogInRestaurantController.java
@@ -0,0 +1,21 @@
+package labTic.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LogInRestaurantController {
+
+    @FXML
+    private TextField rut;
+
+    @FXML
+    private PasswordField pass;
+
+    @FXML
+    void btnLogin(MouseEvent event) {
+
+    }
+
+}
