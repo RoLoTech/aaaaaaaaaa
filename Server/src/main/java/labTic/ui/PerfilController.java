81892ef485ce075d0f0e1f2b29e51dabccfa44cc
Fernando Badano <ferbadano20@gmail.com>
2018-11-04 00:56:03 -0300

    GUI Perfil

---
+++ Server/src/main/java/labTic/ui/PerfilController.java
@@ -0,0 +1,37 @@
+package labTic.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class PerfilController {
+
+    @FXML
+    private TextField houropen;
+
+    @FXML
+    private TextField hourclose;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private TextField address;
+
+    @FXML
+    private TextField image;
+
+    @FXML
+    void perfilTab(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reservasTab(MouseEvent event) {
+
+    }
+
+}
