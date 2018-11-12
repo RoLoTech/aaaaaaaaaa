143ed238c120653b99b9900c9f19cdd792001974
Fernando Badano <ferbadano20@gmail.com>
2018-11-12 20:54:15 -0300

    GUI Reservas

---
+++ Server/src/main/java/labTic/ui/ReservaConfirmadaController.java
@@ -0,0 +1,39 @@
+package labTic.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class ReservaConfirmadaController {
+
+    @FXML
+    private ImageView imgRestaurant;
+
+    @FXML
+    private Text txtRestaurant;
+
+    @FXML
+    private Text txtHora;
+
+    @FXML
+    private Text txtUbicacion;
+
+    @FXML
+    private Text txtPersonas;
+
+    @FXML
+    private Text txtTelefono;
+
+    @FXML
+    private Text txtDireccion;
+
+    @FXML
+    private Text txtMesa;
+
+    @FXML
+    void btnVolver(MouseEvent event) {
+
+    }
+
+}
---
+++ Server/src/main/java/labTic/ui/ReservaPendienteController.java
@@ -0,0 +1,41 @@
+package labTic.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class ReservaPendienteController {
+
+    @FXML
+    private ImageView imgRestaurant;
+
+    @FXML
+    private ImageView imgLoading;
+
+    @FXML
+    private Text txtRestaurant;
+
+    @FXML
+    private Text txtUbicacion;
+
+    @FXML
+    private Text txtPersonas;
+
+    @FXML
+    private Text txtTelefono;
+
+    @FXML
+    private Text txtDireccion;
+
+    @FXML
+    void btnCancelar(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnVolver(MouseEvent event) {
+
+    }
+
+}
