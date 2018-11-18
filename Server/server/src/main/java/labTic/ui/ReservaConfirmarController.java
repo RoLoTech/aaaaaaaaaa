7030b2ac725cacd97aa9abdf5008672e5d7de94d
Fernando Badano <ferbadano20@gmail.com>
2018-11-18 15:19:08 -0300

    GUI Arreglos

---
+++ Server/server/src/main/java/labTic/ui/ReservaConfirmarController.java
@@ -0,0 +1,40 @@
+package labTic.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class ReservaConfirmarController {
+
+    @FXML
+    private Text txtNombre;
+
+    @FXML
+    private Text txtPersonas;
+
+    @FXML
+    private MenuButton selectorMesa;
+
+    @FXML
+    void btnConfirmar(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnFiltroMultiple1(ContextMenuEvent event) {
+
+    }
+
+    @FXML
+    void perfilTab(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reservasTab(MouseEvent event) {
+
+    }
+
+}
